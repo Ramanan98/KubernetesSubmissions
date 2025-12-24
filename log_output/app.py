e80d059ac94a1447dd7814f8a0ce2361fc1e4c6d
Ramanan <ramanan.murugesan@gmail.com>
2025-12-24 14:17:06 +0100

    ex-1.7

--- log_output/app.py
+++ log_output/app.py
@@ -1,9 +1,20 @@
-import time
+import os
 import random
 import string
 from datetime import datetime
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-s = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
-while True:
-    print(datetime.now().isoformat(), s, flush=True)
-    time.sleep(5)
+s = "".join(random.choices(string.ascii_letters + string.digits, k=12))
+port = int(os.environ.get("PORT", 8080))
+print(f"Server started on port {port}", flush=True)
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        response = f"{datetime.now().isoformat()} {s}"
+        self.wfile.write(response.encode())
+
+
+HTTPServer(("", port), Handler).serve_forever()
