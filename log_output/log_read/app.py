dfb1359e07741ec69d3e728c4e88ab9c644ac3b5
Ramanan <ramanan.murugesan@gmail.com>
2025-12-24 20:42:18 +0100

    ex-1.10

--- log_output/log_read/app.py
+++ log_output/log_read/app.py
@@ -1,10 +1,6 @@
 import os
-import random
-import string
-from datetime import datetime
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-s = "".join(random.choices(string.ascii_letters + string.digits, k=12))
 port = int(os.environ.get("PORT", 8080))
 print(f"Server started on port {port}", flush=True)
 
@@ -13,7 +9,8 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        response = f"{datetime.now().isoformat()} {s}"
+        with open("/usr/src/app/files/log.txt", "r") as f:
+            response = f.read()
         self.wfile.write(response.encode())
 
 
