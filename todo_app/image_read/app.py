42867d77de14d5d1849939a46e6f6a0ec6a7be90
Ramanan <ramanan.murugesan@gmail.com>
2025-12-25 12:14:55 +0100

    ex-1.13

--- todo_app/image_read/app.py
+++ todo_app/image_read/app.py
@@ -2,41 +2,28 @@ import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 port = int(os.environ.get("PORT", 8080))
-print(f"Server started in port {port}", flush=True)
+print(f"Server started on port {port}", flush=True)
 
 IMAGE_PATH = "/usr/src/app/files/image.jpg"
+HTML_FILE = "/app/index.html"
 
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/image.jpg":
-            try:
-                with open(IMAGE_PATH, "rb") as f:
-                    data = f.read()
-
+            with open(IMAGE_PATH, "rb") as f:
+                data = f.read()
+            self.send_response(200)
+            self.send_header("Content-Type", "image/jpeg")
+            self.end_headers()
+            self.wfile.write(data)
+        else:
+            with open(HTML_FILE, "rb") as f:
+                html_data = f.read()
                 self.send_response(200)
-                self.send_header("Content-Type", "image/jpeg")
+                self.send_header("Content-Type", "text/html")
                 self.end_headers()
-                self.wfile.write(data)
-            except FileNotFoundError:
-                self.send_response(404)
-                self.end_headers()
-            return
-
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.end_headers()
-        self.wfile.write(
-            """
-            <html>
-                <body>
-                    <h1>The project app</h1>
-                    <img src="/image.jpg" />
-                    <p>DevOps with Kubernetes 2025</p>
-                </body>
-            </html>
-        """.encode("utf-8")
-        )
+                self.wfile.write(html_data)
 
 
 HTTPServer(("", port), Handler).serve_forever()
