abb8996c368257a8954ae8610953a664a4d73abd
Ramanan <ramanan.murugesan@gmail.com>
2025-12-25 20:24:30 +0100

    ex-2.5

--- README.md
+++ README.md
@@ -24,3 +24,4 @@
 - [2.2](https://github.com/Ramanan98/KubernetesSubmissions/tree/2.2/todo_app)
 - [2.3](https://github.com/Ramanan98/KubernetesSubmissions/tree/2.3)
 - [2.4](https://github.com/Ramanan98/KubernetesSubmissions/tree/2.4/todo_app)
+- [2.5](https://github.com/Ramanan98/KubernetesSubmissions/tree/2.5/log_output)
--- log_output/app.py
+++ log_output/app.py
@@ -7,6 +7,10 @@ import http.client
 
 s = "".join(random.choices(string.ascii_letters + string.digits, k=12))
 port = int(os.environ.get("PORT", 8080))
+
+MESSAGE = os.environ.get("MESSAGE", "")
+INFO_FILE = "/config/information.txt"
+
 print(f"Server started on port {port}", flush=True)
 
 
@@ -18,11 +22,20 @@ class Handler(BaseHTTPRequestHandler):
         ping_pong_requests = res.read().decode()
         conn.close()
 
+        file_content = ""
+        with open(INFO_FILE, "r") as f:
+            file_content = f.read().strip()
+
         self.send_response(200)
         self.end_headers()
+
         response = (
-            f"{datetime.now().isoformat()} {s}\nPing / Pongs: {ping_pong_requests}"
+            f"{file_content}\n"
+            f"{MESSAGE}\n"
+            f"{datetime.now().isoformat()}: {s}.\n"
+            f"Ping / Pongs: {ping_pong_requests}"
         )
+
         self.wfile.write(response.encode())
 
 
