7ed7f5a7045da977f6b41034fd81b1da1a81d5b2
Ramanan <ramanan.murugesan@gmail.com>
2025-12-25 21:49:33 +0100

    ex-2.7

--- README.md
+++ README.md
@@ -26,3 +26,4 @@
 - [2.4](https://github.com/Ramanan98/KubernetesSubmissions/tree/2.4/todo_app)
 - [2.5](https://github.com/Ramanan98/KubernetesSubmissions/tree/2.5/log_output)
 - [2.6](https://github.com/Ramanan98/KubernetesSubmissions/tree/2.6/todo_app)
+- [2.7](https://github.com/Ramanan98/KubernetesSubmissions/tree/2.7)
--- ping_pong/app.py
+++ ping_pong/app.py
@@ -1,5 +1,31 @@
 import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import psycopg2
+
+DB_HOST = os.environ.get("POSTGRES_HOST", "postgres-svc")
+DB_PORT = int(os.environ.get("POSTGRES_PORT", 5432))
+DB_NAME = os.environ.get("POSTGRES_DB", "postgres")
+DB_USER = os.environ.get("POSTGRES_USER", "postgres")
+DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "")
+
+conn = psycopg2.connect(
+    host=DB_HOST,
+    port=DB_PORT,
+    database=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+)
+conn.autocommit = True
+cur = conn.cursor()
+
+print("Connected to Postgres", flush=True)
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS ping_counter (
+    id SERIAL PRIMARY KEY,
+    counter INT
+)
+""")
 
 port = int(os.environ.get("PORT", 8080))
 print(f"Server started on port {port}", flush=True)
@@ -18,6 +44,9 @@ class Handler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(f"pong {Handler.counter}".encode())
             Handler.counter += 1
+            cur.execute(
+                "INSERT INTO ping_counter(counter) VALUES (%s)", (Handler.counter,)
+            )
 
 
 HTTPServer(("", port), Handler).serve_forever()
